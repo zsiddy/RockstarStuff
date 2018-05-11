6142504ae4f47b69e57ac59418ecba0cdc31c1c3
zsiddy <39176343+zsiddy@users.noreply.github.com>
2018-05-10 18:16:01 -0700

    Add files via upload

---
+++ TriviaGameTest.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class TriviaGameTest here.
+ * 
+ * @author Zoe Siddall & Cameron Smith 
+ * @version V1
+ */
+public class TriviaGameTest
+{
+    public static void main(String[] args)
+    {
+        TriviaGame game1 = new TriviaGame();
+        
+        game1.playGame();
+    }
+}
